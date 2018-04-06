c7258177fecb90df4da8cab6b76e08c7f7868ace
Michael Kovich <kovich@michaelkovich.com>
2018-04-06 13:44:23 -0500

    Homepage design adjustments

--- src/components/subcomponents/Button.js
+++ src/components/subcomponents/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function button(props) {
   return (
-    <button className="btn btn-outline-secondary" onClick={props.click}>
+    <button className="btn btn-outline-light" onClick={props.click}>
       {props.title}
     </button>
   );
