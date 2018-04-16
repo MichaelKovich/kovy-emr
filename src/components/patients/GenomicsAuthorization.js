a988fe670e019fa5c4051748b9576203a0c43249
Michael Kovich <kovich@michaelkovich.com>
2018-04-15 21:20:00 -0500

    Adjusted TTAM API endpoint.

--- src/components/patients/GenomicsAuthorization.js
+++ src/components/patients/GenomicsAuthorization.js
@@ -31,7 +31,7 @@ class GenomicsAuthorization extends Component {
                 your Genetic Phenotype Range Interactions right on this page! Click the button below
                 to get started.
               </p>
-              <a href="https://api.23andme.com/authorize/?redirect_uri=http://localhost:3000/receive_code/&response_type=code&client_id=8dbcf90617c73ea960abe7e69e00dae4&scope=report:all&select_profile=True">
+              <a href="https://api.23andme.com/authorize/?redirect_uri=http://iyashiemr.com/receive_code/&response_type=code&client_id=8dbcf90617c73ea960abe7e69e00dae4&scope=report:all&select_profile=True">
                 <img
                   src="https://api.23andme.com/res/img/connect-button-example.0e3585418be8.png"
                   alt="Connect With 23AndMe"
